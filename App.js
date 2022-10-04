6aa350bdd8653967fe2b9764d114bad5d157ee82
p-nayanjyoti <103568269+p-nayanjyoti@users.noreply.github.com>
2022-10-04 23:04:28 +0530

    Add files via upload

--- App.js
+++ App.js
@@ -1,17 +1,21 @@
 import "./App.css";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import ReactDOM from "react-dom/client";
 import Create from "./Components/Create";
 import Read from "./Components/Read";
 import Update from "./Components/Update";
+import React from "react";
+import {  BrowserRouter,Routes,Route} from "react-router-dom";
 
 function App() {
   return (
     <div className="container">
       <BrowserRouter>
         <Routes>
+
           <Route exact path="/" element={<Create />}></Route>
-          <Route path="/read" element={<Read />}></Route>
-          <Route path="/update" element={<Update />}></Route>
+          <Route exact path="/read" element={<Read />}></Route>
+          <Route exact path="/update" element={<Update />}></Route>
+
         </Routes>
       </BrowserRouter>
     </div>
